2a0bc5291c623a6e11f4d04b9e438326bf0069dd
Jason Roberts <osjasonroberts@gmail.com>
2013-05-14 08:15:05 +0800

    test

---
+++ WebApplication1/Unit.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Unit.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        
+        [Fact]
+        [Trait("category","smoke")]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
